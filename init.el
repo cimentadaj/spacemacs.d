b42c9ed705819d7390d46a34f4947e9a3108d5bf
Jorge Cimentada <cimentadaj@gmail.com>
2021-04-08 10:59:58 +0200

    Adds env variables for pipenv

--- init.el
+++ init.el
@@ -610,7 +610,8 @@ This function defines the environment variables for your Emacs session. By
 default it calls `spacemacs/load-spacemacs-env' which loads the environment
 variables declared in `~/.spacemacs.env' or `~/.spacemacs.d/.spacemacs.env'.
 See the header of this file for more information."
-
+  (setenv "PIPENV_VENV_IN_PROJECT" "1")
+  (setenv "PIPENV_VERBOSITY" "-1")
   )
 
 (defun custom//load-all ()
