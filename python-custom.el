52e18788ce459875e3bc0f7bd721005244717a6d
Jorge Cimentada <cimentadaj@gmail.com>
2021-05-19 09:37:00 +0200

    Adds python customization

--- python-custom.el
+++ python-custom.el
@@ -1,5 +1,14 @@
 
 (defun custom/python-config ()
+
+  (defun python-insert-assign ()
+    "Insert an assignment ="
+    (interactive)
+    (insert " = "))
+
+  (define-key python-mode-map (kbd "C-<") 'python-insert-assign)
+  (define-key inferior-python-mode-map (kbd "C-<") 'python-insert-assign)
   (spacemacs/declare-prefix-for-mode 'python-mode "E" "Extra")
   (spacemacs/set-leader-keys-for-major-mode 'python-mode "Er" 'pyvenv-restart-python)
+
   )
